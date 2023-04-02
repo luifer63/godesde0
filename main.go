bdf16696d6b940cc02cc02d5f072c7ebc4c0afb9
luifer63 <lvix2000@gmail.com>
2023-04-01 20:25:56 -0500

    Otras variables y funciones

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/luifer63/godesde0/variables"
 )
 
 // funcion principal de go
 func main(){
-	variables.MuestroEnteros()	
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
+
 }
---
+++ variables/resto.go
@@ -0,0 +1,31 @@
+package variables
+
+import (
+	"fmt"
+	"time"
+	"strconv"
+)
+
+var Nombre string
+var Estado bool
+var Sueldo float32
+var Fecha time.Time
+
+
+func RestoVariables(){
+	Nombre = "Luis"
+	Estado = true
+	Sueldo = 1577.66
+	Fecha = time.Now()
+
+	fmt.Println(Nombre)
+	fmt.Println(Estado)
+	fmt.Println(Sueldo)
+	fmt.Println(Fecha)	
+}
+
+func ConviertoaTexto(numero int) (bool, string){
+	//var texto string
+	texto := strconv.Itoa(numero)
+	return true, texto
+}
